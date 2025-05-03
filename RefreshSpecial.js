693f520810fc714d8d81375085336e8ab8adc069
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2025-05-03 06:31:19 +0000

    build: Updating eslint-config-wikimedia to 0.29.1
    Additional changes:
    * eslint: Replaced `wikimedia/client-es5` with `wikimedia/client`.
    
    Change-Id: Ib4915415984c8fe4aadd2181dec92611db7b200f

--- RefreshSpecial.js
+++ RefreshSpecial.js
@@ -1,7 +1,7 @@
 /**
  * JavaScript helper function for RefreshSpecial extension
  */
-$( function () {
+$( () => {
 	$( 'input#refreshSpecialCheckAll' ).on( 'click', function () {
 		$( 'input[name="wpSpecial\\[\\]"]' ).prop( 'checked', $( this ).prop( 'checked' ) );
 	} );
